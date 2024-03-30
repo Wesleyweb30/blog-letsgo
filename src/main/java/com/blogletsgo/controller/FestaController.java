8fb8e31afd28d8a207ed28ba271b77bbf07516e9
Wesley <wesleyweb3.0@gmail.com>
2024-03-30 19:45:25 -0300

    liberando acesso para o react para festas controller

--- src/main/java/com/blogletsgo/controller/FestaController.java
+++ src/main/java/com/blogletsgo/controller/FestaController.java
@@ -3,22 +3,18 @@ package com.blogletsgo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import com.blogletsgo.model.Festa;
 import com.blogletsgo.repository.FestaRepository;
-
-import jakarta.websocket.server.PathParam;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("/festas")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class FestaController {
 
     @Autowired
@@ -34,5 +30,5 @@ public class FestaController {
     public Festa postMethodName(@RequestParam Festa entity) {
         return repository.save(entity);
     }
-    
+
 }
